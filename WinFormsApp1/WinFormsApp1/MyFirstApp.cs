c583c64aa51172fc7e5613457c434ddc97d68a8c
Florian Stettler <florianstettler95.gmail.com@zbw-online.ch>
2025-10-25 08:30:01 +0200

    Projekt unbenant

---
+++ WinFormsApp1/WinFormsApp1/MyFirstApp.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp1
+{
+    public partial class MyFirstApp : Form
+    {
+        public MyFirstApp()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
