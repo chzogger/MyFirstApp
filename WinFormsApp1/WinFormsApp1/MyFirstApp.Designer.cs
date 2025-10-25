74957fec466251332d272b4736202133ba46bf8c
Florian Stettler <florianstettler95.gmail.com@zbw-online.ch>
2025-10-25 11:14:54 +0200

    Aufgabe A1-2-1 fertig

--- WinFormsApp1/WinFormsApp1/MyFirstApp.Designer.cs
+++ WinFormsApp1/WinFormsApp1/MyFirstApp.Designer.cs
@@ -28,37 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            CmdHello = new Button();
-            LblDisplay = new Label();
+            CmdButtonYellow = new Button();
+            CmdButtonBlue = new Button();
+            CmdButtonRed = new Button();
+            CmdButtonGreen = new Button();
+            feld1 = new Label();
+            feld2 = new Label();
+            feld3 = new Label();
+            feld4 = new Label();
             SuspendLayout();
             // 
-            // CmdHello
+            // CmdButtonYellow
             // 
-            CmdHello.Location = new Point(37, 32);
-            CmdHello.Name = "CmdHello";
-            CmdHello.Size = new Size(147, 79);
-            CmdHello.TabIndex = 0;
-            CmdHello.Text = "Hello";
-            CmdHello.UseVisualStyleBackColor = true;
-            CmdHello.Click += CmdHello_Click;
+            CmdButtonYellow.Location = new Point(37, 32);
+            CmdButtonYellow.Name = "CmdButtonYellow";
+            CmdButtonYellow.Size = new Size(147, 79);
+            CmdButtonYellow.TabIndex = 0;
+            CmdButtonYellow.Text = "Gelb";
+            CmdButtonYellow.UseVisualStyleBackColor = true;
+            CmdButtonYellow.Click += CmdHello_Click;
             // 
-            // LblDisplay
+            // CmdButtonBlue
             // 
-            LblDisplay.AutoSize = true;
-            LblDisplay.Location = new Point(470, 264);
-            LblDisplay.Name = "LblDisplay";
-            LblDisplay.Size = new Size(54, 32);
-            LblDisplay.TabIndex = 1;
-            LblDisplay.Text = "........";
-            LblDisplay.Click += label1_Click;
+            CmdButtonBlue.Location = new Point(37, 361);
+            CmdButtonBlue.Name = "CmdButtonBlue";
+            CmdButtonBlue.Size = new Size(147, 79);
+            CmdButtonBlue.TabIndex = 2;
+            CmdButtonBlue.Text = "Blau";
+            CmdButtonBlue.UseVisualStyleBackColor = true;
+            CmdButtonBlue.Click += CmdButtonBlue_Click;
+            // 
+            // CmdButtonRed
+            // 
+            CmdButtonRed.Location = new Point(37, 137);
+            CmdButtonRed.Name = "CmdButtonRed";
+            CmdButtonRed.Size = new Size(147, 79);
+            CmdButtonRed.TabIndex = 3;
+            CmdButtonRed.Text = "Rot";
+            CmdButtonRed.UseVisualStyleBackColor = true;
+            CmdButtonRed.Click += CmdButtonRed_Click;
+            // 
+            // CmdButtonGreen
+            // 
+            CmdButtonGreen.Location = new Point(37, 241);
+            CmdButtonGreen.Name = "CmdButtonGreen";
+            CmdButtonGreen.Size = new Size(147, 79);
+            CmdButtonGreen.TabIndex = 4;
+            CmdButtonGreen.Text = "Grün";
+            CmdButtonGreen.UseVisualStyleBackColor = true;
+            CmdButtonGreen.Click += CmdButtonGreen_Click;
+            // 
+            // feld1
+            // 
+            feld1.AutoSize = true;
+            feld1.Location = new Point(266, 79);
+            feld1.Name = "feld1";
+            feld1.Size = new Size(0, 32);
+            feld1.TabIndex = 5;
+            feld1.Click += label1_Click_1;
+            // 
+            // feld2
+            // 
+            feld2.AutoSize = true;
+            feld2.Location = new Point(266, 184);
+            feld2.Name = "feld2";
+            feld2.Size = new Size(0, 32);
+            feld2.TabIndex = 6;
+            feld2.Click += feld2_Click;
+            // 
+            // feld3
+            // 
+            feld3.AutoSize = true;
+            feld3.Location = new Point(266, 288);
+            feld3.Name = "feld3";
+            feld3.Size = new Size(0, 32);
+            feld3.TabIndex = 7;
+            // 
+            // feld4
+            // 
+            feld4.AutoSize = true;
+            feld4.Location = new Point(266, 408);
+            feld4.Name = "feld4";
+            feld4.Size = new Size(0, 32);
+            feld4.TabIndex = 8;
             // 
             // MyFirstApp
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(LblDisplay);
-            Controls.Add(CmdHello);
+            ClientSize = new Size(895, 595);
+            Controls.Add(feld4);
+            Controls.Add(feld3);
+            Controls.Add(feld2);
+            Controls.Add(feld1);
+            Controls.Add(CmdButtonGreen);
+            Controls.Add(CmdButtonRed);
+            Controls.Add(CmdButtonBlue);
+            Controls.Add(CmdButtonYellow);
             Name = "MyFirstApp";
             Text = "MyFirstApp";
             Load += Form1_Load;
@@ -68,7 +134,13 @@
 
         #endregion
 
-        private Button CmdHello;
-        private Label LblDisplay;
+        private Button CmdButtonYellow;
+        private Button CmdButtonBlue;
+        private Button CmdButtonRed;
+        private Button CmdButtonGreen;
+        private Label feld1;
+        private Label feld2;
+        private Label feld3;
+        private Label feld4;
     }
 }
--- WinFormsApp1/WinFormsApp1/MyFirstApp.cs
+++ WinFormsApp1/WinFormsApp1/MyFirstApp.cs
@@ -19,7 +19,38 @@ namespace WinFormsApp1
 
         private void CmdHello_Click(object sender, EventArgs e)
         {
-            LblDisplay.Text = "Hello";
+            feld1.Text = "Gelbe Hintergrundfarbe";
+            feld1.BackColor = Color.Yellow;
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void feld2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdButtonRed_Click(object sender, EventArgs e)
+        {
+            feld2.Text = "Rote Hintergrundfarve";
+            feld2.BackColor = Color.Red;
+        }
+
+        private void CmdButtonGreen_Click(object sender, EventArgs e)
+        {
+            feld3.Text = "Grüner Hintergrund";
+            feld3.BackColor = Color.Green;
+
+        }
+
+        private void CmdButtonBlue_Click(object sender, EventArgs e)
+        {
+            feld4.Text = "Blauer Hintergrund";
+            feld4.BackColor = Color.Blue;
         }
     }
 }
