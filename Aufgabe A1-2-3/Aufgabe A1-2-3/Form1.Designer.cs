97421dfa30e8e1cb952c57d119ac6bb1f9727d0c
Florian Stettler <florianstettler95.gmail.com@zbw-online.ch>
2025-10-26 11:55:51 +0100

    A1-2-3 fertig

--- Aufgabe A1-2-3/Aufgabe A1-2-3/Form1.Designer.cs
+++ Aufgabe A1-2-3/Aufgabe A1-2-3/Form1.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            HomeButton = new Button();
+            DownButton = new Button();
+            RightButton = new Button();
+            UpButton = new Button();
+            LeftButton = new Button();
+            Field1Button = new Button();
+            SuspendLayout();
+            // 
+            // HomeButton
+            // 
+            HomeButton.Location = new Point(325, 116);
+            HomeButton.Name = "HomeButton";
+            HomeButton.Size = new Size(150, 46);
+            HomeButton.TabIndex = 0;
+            HomeButton.Text = "Home";
+            HomeButton.UseVisualStyleBackColor = true;
+            HomeButton.Click += HomeButton_Click;
+            // 
+            // DownButton
+            // 
+            DownButton.Location = new Point(325, 168);
+            DownButton.Name = "DownButton";
+            DownButton.Size = new Size(150, 46);
+            DownButton.TabIndex = 1;
+            DownButton.Text = "Down";
+            DownButton.UseVisualStyleBackColor = true;
+            DownButton.Click += DownButton_Click_1;
+            // 
+            // RightButton
+            // 
+            RightButton.Location = new Point(481, 116);
+            RightButton.Name = "RightButton";
+            RightButton.Size = new Size(150, 46);
+            RightButton.TabIndex = 2;
+            RightButton.Text = "Right";
+            RightButton.UseVisualStyleBackColor = true;
+            RightButton.Click += RightButton_Click;
+            // 
+            // UpButton
+            // 
+            UpButton.Location = new Point(325, 64);
+            UpButton.Name = "UpButton";
+            UpButton.Size = new Size(150, 46);
+            UpButton.TabIndex = 3;
+            UpButton.Text = "Up";
+            UpButton.UseVisualStyleBackColor = true;
+            UpButton.Click += UpButton_Click;
+            // 
+            // LeftButton
+            // 
+            LeftButton.Location = new Point(169, 116);
+            LeftButton.Name = "LeftButton";
+            LeftButton.Size = new Size(150, 46);
+            LeftButton.TabIndex = 4;
+            LeftButton.Text = "Left";
+            LeftButton.UseVisualStyleBackColor = true;
+            LeftButton.Click += LeftButton_Click_1;
+            // 
+            // Field1Button
+            // 
+            Field1Button.BackColor = Color.RoyalBlue;
+            Field1Button.Location = new Point(159, 299);
+            Field1Button.Name = "Field1Button";
+            Field1Button.Size = new Size(52, 46);
+            Field1Button.TabIndex = 5;
+            Field1Button.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 646);
+            Controls.Add(Field1Button);
+            Controls.Add(LeftButton);
+            Controls.Add(UpButton);
+            Controls.Add(RightButton);
+            Controls.Add(DownButton);
+            Controls.Add(HomeButton);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button HomeButton;
+        private Button DownButton;
+        private Button RightButton;
+        private Button UpButton;
+        private Button LeftButton;
+        private Button Field1Button;
     }
 }
--- Aufgabe A1-2-3/Aufgabe A1-2-3/Form1.cs
+++ Aufgabe A1-2-3/Aufgabe A1-2-3/Form1.cs
@@ -1,10 +1,58 @@
+using System;
+using System.Reflection;
+
 namespace Aufgabe_A1_2_3
 {
     public partial class Form1 : Form
     {
+
+
+        Point meinePosition = new Point(374, 387);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+            Field1Button.Location = new Point(374, 387);
+            meinePosition = Field1Button.Location;
+        }
+
+        private void UpButton_Click(object sender, EventArgs e)
+        {
+            meinePosition.Y -= 10;
+            Field1Button.Location = meinePosition;
+
+        }
+
+        private void RightButton_Click(object sender, EventArgs e)
+        {
+            meinePosition.X += 10;
+            Field1Button.Location = meinePosition;
+        }
+
+      
+               
+
+        private void DownButton_Click_1(object sender, EventArgs e)
+        {
+            meinePosition.Y += 10;
+            Field1Button.Location = meinePosition;
+        }
+
+        private void LeftButton_Click_1(object sender, EventArgs e)
+        {
+            meinePosition.X -= 10;
+            Field1Button.Location = meinePosition;
         }
     }
 }
